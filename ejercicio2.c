d0ddda1ffb582382b1051886f0628324e2c8169f
Lara-Godoy <lgodoy@etrr.edu.ar>
2024-05-17 11:59:22 -0300

    ejer 2

--- ejercicio2.c
+++ ejercicio2.c
@@ -5,18 +5,24 @@ main(void){
     float b=13.546; 
     char c='A'; 
     char d='a'; 
-
     printf ("a = %d", a); 
+//decimal
     printf ("\na = %O", a); 
+//octal
     printf ("\na = %X", a); 
+//hexa
     printf ("\na = %f", a); 
+//float
     printf ("\na = %c", a); 
+//caracter
     printf ("\nb = %d", b); 
     printf ("\nb = %5.2d", b); 
     printf ("\nb = %f", b); 
     printf ("\nb = %.1f", b); 
+//flotante con un numero despues de la coma
     printf ("\nb = %.2f", b); 
     printf ("\nb = %6.4f", b); 
+//flotante con 4 numeros despues de la coma
     printf ("\nb = %6.1f", b); 
     printf ("\nb = %c", b); 
     printf ("\nb = %O", b); 
