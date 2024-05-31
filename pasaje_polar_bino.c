c7b711dcb82999b3048b4cb7b9025ccafd675df5
Lara-Godoy <lgodoy@etrr.edu.ar>
2024-05-31 12:17:21 -0300

    situacion dada por chatgpt

--- pasaje_polar_bino.c
+++ pasaje_polar_bino.c
@@ -8,23 +8,18 @@ int main(void){
     double ejeY, ejeX;
     double rad;
     double ejeX_red, ejeY_red;
-
     printf("ingrese el modulo: \n");
     scanf("%d", &mod);
     printf("ingrese el angulo en grados: \n");
     scanf("%d", &ang);
-
     rad = ang * (pi / 180.0);
-
     ejeY = sin(rad)*mod;
     ejeX = cos(rad)*mod;
-
     ejeY_red = round(ejeY);
     ejeX_red = round(ejeX);
-
 //%f --> xq es float
     printf("Parte imaginaria redondeada: %f", ejeY_red);
-    printf("\nParte imaginaria redondeada: %f", ejeX_red);
+    printf("\nParte real redondeada: %f", ejeX_red);
     printf("\n\nParte imaginaria sin redondear: %f", ejeY);
     printf("\nParte real sin redondear: %f", ejeX);
     return(0);
---
+++ situacion_gpt3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+    int cantidad;
+    float precio_pedido;
+    printf("Bienvenidos al Himalaya! \n");
+    printf("Cuantos kg de bananas (55$) desea llevar?: \n");
+    scanf("%d", &cantidad);
+    precio_pedido = cantidad * 55;
+    
+    if (precio_pedido <= 100){
+        printf("Debes abonar: %d", precio_pedido);
+    }else if ((100 < precio_pedido)&&(precio_pedido < 500)){
+        precio_pedido = (10 * precio_pedido)/100;
+        printf("Debes abonar %f", precio_pedido);
+    }else{
+        printf("%f",precio_pedido);
+        precio_pedido = (20 * precio_pedido)/100;
+        printf("Debes abonar %f", precio_pedido);
+    }
+}
+
+/*Si el total de la compra es menor o igual a $100, no se aplica ningún descuento.
+Si el total de la compra está entre $100 y $500, se aplica un descuento del 10%.
+Si el total de la compra es mayor a $500, se aplica un descuento del 20%.*/
