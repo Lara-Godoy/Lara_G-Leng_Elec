fdd30bc45f8a6a350c02ef7cf6fa2fc14f6b8e5d
Lara-Godoy <lgodoy@etrr.edu.ar>
2024-05-31 12:38:15 -0300

    codigos

--- sit_gpt3_change.c
+++ sit_gpt3_change.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
 int main(void){
-    int cantidad;
+    int cantidad, precio;
     float precio_pedido;
     printf("Bienvenidos al Himalaya! \n");
-    printf("Cuantos kg de bananas (55$) desea llevar?: \n");
+    printf("Cuantos kg de banana desea llevar?: \n");
     scanf("%d", &cantidad);
-    precio_pedido = cantidad * 55;
+    printf("A cuanto esta el kg de bananas?: \n");
+    scanf("%d", &precio);
+    precio_pedido = cantidad * precio;
     
     if (precio_pedido <= 100){
         printf("Debes abonar: %d", precio_pedido, "pesos");
     }else if ((100 < precio_pedido)&&(precio_pedido < 500)){
         precio_pedido = (10 * precio_pedido)/100;
-        printf("Debes abonar: %f", precio_pedido, "pesos");
+        printf("Debes abonar: %f", precio_pedido), "pesos";
     }else{
         precio_pedido = (20 * precio_pedido)/100;
         printf("Debes abonar: %f", precio_pedido, "pesos");
     }
 }
-
-/*Si el total de la compra es menor o igual a $100, no se aplica ningún descuento.
-Si el total de la compra está entre $100 y $500, se aplica un descuento del 10%.
-Si el total de la compra es mayor a $500, se aplica un descuento del 20%.*/
---
+++ situacion_gpt4.c
@@ -0,0 +1,20 @@
+/*El programa debe pedir al usuario que ingrese una calificación.
+Después de ingresar cada calificación, el programa debe preguntar si el usuario desea ingresar otra 
+calificación.
+Si el usuario decide no ingresar más calificaciones, el programa debe calcular y mostrar el promedio de 
+las calificaciones ingresadas.*/
+
+#include <stdio.h>
+
+int main(void){
+    int notas_dadas;
+    float cali_1;
+    printf("Ingresa una calificacion: \n");
+    scanf("%f", cali_1);
+    printf("Ingresa las notas que te dieron: \n");
+    scanf("%d", &notas_dadas);
+    while (notas_dadas < ){
+        /* code */
+    }
+    
+}
