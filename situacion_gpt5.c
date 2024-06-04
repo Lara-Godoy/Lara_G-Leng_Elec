0107db476b17a1678c79275c3d95cc056ca2b17c
Lara-Godoy <lgodoy@etrr.edu.ar>
2024-06-04 10:52:24 -0300

    le puse un contador

--- situacion_gpt5.c
+++ situacion_gpt5.c
@@ -6,21 +6,22 @@ Repetir el proceso hasta que el usuario adivine correctamente el número secreto
 #include <stdio.h>
 
 int main(void){
-    int num_secreto = 3587;
-    int num_usuario;
+    int num_secreto = 4298;
+    int num_usuario, contador = 1;
 
     printf("Ingresa tu intento para adivinar un numero secreto: \n");
     scanf("%d", &num_usuario);
     while (num_usuario != num_secreto){
-        if (num_usuario < num_secreto)
-        {
+        if (num_usuario < num_secreto){
             printf("Error, intenta de nuevo un numero mayor: \n");
             scanf("%d", &num_usuario);
         }else{
             printf("Error, intenta de nuevo un numero menor: \n");
             scanf("%d", &num_usuario);
         }
+    contador = contador + 1;
     }
-    printf("Bien ahi, master, correcto.");
+    printf("Bien ahi, master, correcto. \n");
+    printf("Intentaste: %d", contador, "veces.");
     return 0;
 }
