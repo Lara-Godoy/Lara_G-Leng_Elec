64357e9281fc7671ba7207f591097816711e2373
Lara-Godoy <lgodoy@etrr.edu.ar>
2024-06-07 14:44:43 -0300

    codigo

---
+++ chat gpt situaciones/situacion_gpt7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int num_user_min, num_user_max;
+    int suma_pares = 0;
+
+    printf("Ingresa tu numero de inicio: \n");
+    scanf("%d", &num_user_min);
+    printf("Ingresa tu numero final: \n");
+    scanf("%d", &num_user_max);
+
+    for(num_user_min; num_user_min < num_user_max; num_user_min++){
+        if (num_user_min % 2 == 0){(suma_pares += num_user_min);}
+    }
+    printf("Suma: %d", suma_pares);
+    return 0;
+}
