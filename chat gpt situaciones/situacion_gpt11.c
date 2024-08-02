a69c860659ca880187ae523858b4e9fec4d6e547
Lara-Godoy <lgodoy@etrr.edu.ar>
2024-08-02 14:05:32 -0300

    codigos

--- chat gpt situaciones/situacion_gpt11.c
+++ chat gpt situaciones/situacion_gpt11.c
@@ -1,18 +1,71 @@
 #include <stdio.h>
 
-float suma(int, int);
-float resta(int, int);
-float multi(int, int);
-float div(int, int);
+int suma(int a, int b);
+int resta(int a, int b);
+int multi(int a, int b);
+int div(int a, int b);
 
 int main(){
     int num_1, num_2;
-    int resultado;
+    int resultado, eleccion;
 
-    printf("Ingrese el primer número: ");
-    scanf("%f", &num1);
-    printf("Ingrese el segundo número: ");
-    scanf("%f", &num2);
+    printf("Bienvenido. Que deseas hacer?: \n");
+    printf("1. Suma. \n");
+    printf("2. Resta. \n");
+    printf("3. Multiplicacion. \n");
+    printf("4. Division. \n");
+    scanf("%d", &eleccion);
 
+    printf("Ingrese el primer numero: ");
+    scanf("%d", &num_1);
+    printf("Ingrese el segundo numero: ");
+    scanf("%d", &num_2);
+
+    switch (eleccion)
+    {
+    case 1:
+        resultado = suma(num_1, num_2);
+        printf("\nResultado: %d", resultado);
+        break;
+
+    case 2:
+        resultado = resta(num_1, num_2);
+        printf("\nResultado: %d", resultado);
+        break;
     
+    case 3:
+        resultado = multi(num_1, num_2);
+        printf("\nResultado: %d", resultado);
+        break;
+    
+    case 4:
+        if (num_2 != 0){
+            resultado = div(num_1, num_2);
+            printf("\nResultado: %d", resultado);
+        }else{
+            printf("No se puede dividir por 0");
+        }
+        break;
+
+    default:
+            printf("Operación no válida.\n");
+            break;
+    return 0;
+    }
+}
+
+int suma(int a, int b){
+    return a + b;
+}
+
+int resta(int a, int b){
+    return a - b;
+}
+
+int multi(int a, int b){
+    return a * b;
+}
+
+int div(int a, int b){
+    return a / b;
 }
